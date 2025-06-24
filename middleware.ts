9a571b6962c357dc733ddea8aa0b5771db776e9b
Oleksandr Kurzhy <okurzhy@fexillon.com>
2025-06-24 11:06:40 +0300

    cooki name correct

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.AUTH_SECRET,
-    cookieName: "__Secure-next-auth.session-token",
+    cookieName: "__Secure-authjs.session-token",
   });
 
   console.log({ token });
