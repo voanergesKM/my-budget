520c10c95033a64523c7ff48b683654cdad79fad
Oleksandr Kurzhy <ahilles.km@gmail.com>
2025-07-29 15:53:17 +0300

    updated matcher config

--- middleware.ts
+++ middleware.ts
@@ -27,6 +27,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.[^/]+$).*)"],
 };
