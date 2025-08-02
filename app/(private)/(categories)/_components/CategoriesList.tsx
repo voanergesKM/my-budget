001eb047c21a4c3d504125aa08d3bbbed340a14a
Oleksandr Kurzhy <ahilles.km@gmail.com>
2025-08-02 14:01:47 +0300

    fixed issue with hidden create category button

--- app/(private)/(categories)/_components/CategoriesList.tsx
+++ app/(private)/(categories)/_components/CategoriesList.tsx
@@ -50,10 +50,10 @@ const CategoriesList = () => {
           <TabsTrigger value="outgoing">Outgoing</TabsTrigger>
           <TabsTrigger value="incoming">Incoming</TabsTrigger>
         </TabsList>
-        {["incoming", "outgoing"].map((origin) => (
+        {["outgoing", "incoming"].map((origin) => (
           <Content
             key={origin}
-            origin={origin as "outgoing" | "incoming"}
+            origin={(origin as "outgoing" | "incoming") || "outgoing"}
             list={data?.data || []}
           />
         ))}
@@ -94,57 +94,55 @@ const Content = ({ origin, list }: ContentProps) => {
       />
 
       <TabsContent value={origin} className="space-y-5">
-        {!!list.length && (
-          <div className="grid gap-4 [grid-template-columns:repeat(auto-fill,minmax(330px,1fr))]">
-            <Card
-              onClick={() => setOpenDialog(true)}
-              className="cursor-py-0 flex cursor-pointer flex-row items-center justify-between"
-            >
-              <CardHeader className="flex flex-row items-center gap-4 py-4">
-                <div className="relative flex h-12 w-12 items-center justify-center rounded-full border md:h-16 md:w-16">
-                  <TriggerIconComponent className="size-6 text-text-primary md:size-8" />
-                </div>
-                <CardTitle className="!m-0 !p-0 text-lg">
-                  Add new category
-                </CardTitle>
-              </CardHeader>
-            </Card>
+        <div className="grid gap-4 [grid-template-columns:repeat(auto-fill,minmax(330px,1fr))]">
+          <Card
+            onClick={() => setOpenDialog(true)}
+            className="cursor-py-0 flex cursor-pointer flex-row items-center justify-between"
+          >
+            <CardHeader className="flex flex-row items-center gap-4 py-4">
+              <div className="relative flex h-12 w-12 items-center justify-center rounded-full border md:h-16 md:w-16">
+                <TriggerIconComponent className="size-6 text-text-primary md:size-8" />
+              </div>
+              <CardTitle className="!m-0 !p-0 text-lg">
+                Add new category
+              </CardTitle>
+            </CardHeader>
+          </Card>
 
-            {list.map((category: Category) => {
-              const IconComponent =
-                categoryIcons[category.icon as keyof typeof categoryIcons];
+          {list.map((category: Category) => {
+            const IconComponent =
+              categoryIcons[category.icon as keyof typeof categoryIcons];
 
-              return (
-                <Card
-                  key={category._id}
-                  className="cursor-py-0 flex flex-row items-center justify-between"
-                >
-                  <CardHeader className="flex flex-row items-center gap-4 py-4">
-                    <div
-                      className="relative flex h-12 w-12 items-center justify-center rounded-full md:h-16 md:w-16"
-                      style={{ backgroundColor: category.color }}
-                    >
-                      <IconComponent className="size-6 text-text-primary md:size-8" />
-                    </div>
-                    <CardTitle className="!m-0 !p-0 text-lg">
-                      {category.name}
-                    </CardTitle>
-                  </CardHeader>
-                  <CardContent className="flex flex-row items-center p-0 pr-2">
-                    <Button
-                      variant={"ghost"}
-                      size={"icon"}
-                      className="text-text-primary md:[&_svg]:size-5"
-                      onClick={() => handleEdit(category)}
-                    >
-                      <Edit />
-                    </Button>
-                  </CardContent>
-                </Card>
-              );
-            })}
-          </div>
-        )}
+            return (
+              <Card
+                key={category._id}
+                className="cursor-py-0 flex flex-row items-center justify-between"
+              >
+                <CardHeader className="flex flex-row items-center gap-4 py-4">
+                  <div
+                    className="relative flex h-12 w-12 items-center justify-center rounded-full md:h-16 md:w-16"
+                    style={{ backgroundColor: category.color }}
+                  >
+                    <IconComponent className="size-6 text-text-primary md:size-8" />
+                  </div>
+                  <CardTitle className="!m-0 !p-0 text-lg">
+                    {category.name}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-row items-center p-0 pr-2">
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    className="text-text-primary md:[&_svg]:size-5"
+                    onClick={() => handleEdit(category)}
+                  >
+                    <Edit />
+                  </Button>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
       </TabsContent>
     </>
   );
