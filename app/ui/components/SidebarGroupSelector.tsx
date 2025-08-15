1ca1dff9baf36379efeb098397f81b6132e3701f
Oleksandr Kurzhy <ahilles.km@gmail.com>
2025-08-15 15:55:46 +0300

    sidebar translated

--- app/ui/components/SidebarGroupSelector.tsx
+++ app/ui/components/SidebarGroupSelector.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
 import { useQuery } from "@tanstack/react-query";
 import { Users } from "lucide-react";
 
@@ -10,22 +12,29 @@ import QueryKeys from "@/app/lib/utils/queryKeys";
 import { getUserGroups } from "@/app/lib/api/groups/getUserGroups";
 
 import { Avatar, AvatarImage } from "@/app/ui/shadcn/Avatar";
+import { Button } from "@/app/ui/shadcn/Button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/app/ui/shadcn/DropdownMenu";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/app/ui/shadcn/Popover";
+import { Separator } from "@/app/ui/shadcn/Separator";
 import { useSidebar } from "@/app/ui/shadcn/Sidebar";
+
+import CircularProgress from "@/app/ui/components/CircularProgress";
 
 function SidebarGroupSelector() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const currentGroupId = searchParams.get("groupId");
 
+  const t = useTranslations("Sidebar");
+
   const { isMobile, setOpenMobile } = useSidebar();
 
-  const { data: userGroups } = useQuery({
+  const [open, setopen] = useState(false);
+
+  const { data: userGroups, isLoading } = useQuery({
     queryKey: [QueryKeys.groupsList],
     queryFn: getUserGroups,
   });
@@ -43,6 +52,8 @@ function SidebarGroupSelector() {
       setOpenMobile(false);
     }
 
+    setopen(false);
+
     router.push(`?${params.toString()}`);
   };
 
@@ -51,45 +62,64 @@ function SidebarGroupSelector() {
 
   return (
     <div className="mb-4">
-      <DropdownMenu>
-        <DropdownMenuTrigger className="flex h-[50px] w-full items-center gap-4 rounded-md bg-[var(--button-bg)] px-3 py-3 text-left text-base text-[var(--button-text)] shadow-md hover:bg-[var(--button-hover-bg)] hover:shadow-lg">
-          {currentGroup ? (
-            <Avatar>
-              <AvatarImage
-                src={currentGroup.image || "/image-placeholder.avif"}
-              />
-            </Avatar>
-          ) : (
-            <Users />
-          )}
-
-          <span className="overflow-hidden truncate text-ellipsis text-nowrap">
-            {currentGroup ? currentGroup.name : "My Personal Space"}
-          </span>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-[300px] border-none bg-secondary px-4 py-2 text-text-primary shadow-xl">
-          <DropdownMenuItem
-            onClick={() => handleSelect(null)}
-            className="text-md whitespace-normal break-all pl-3"
+      <Popover open={open} onOpenChange={setopen}>
+        <PopoverTrigger asChild>
+          <Button
+            className="w-full items-center justify-start px-4"
+            onClick={() => setopen(!open)}
+            size={"lg"}
+            isLoading={isLoading}
           >
-            <Users className="mr-1 !h-8 !w-8" />
-            My Personal Space
-          </DropdownMenuItem>
+            {currentGroup ? (
+              <Avatar>
+                <AvatarImage
+                  src={currentGroup.image || "/image-placeholder.avif"}
+                />
+              </Avatar>
+            ) : isLoading ? (
+              <CircularProgress size={100} />
+            ) : (
+              <Users className="!h-6 !w-6" />
+            )}
+
+            <span className="overflow-hidden truncate text-ellipsis text-nowrap">
+              {currentGroup ? currentGroup.name : t("personalWorkspace")}
+            </span>
+          </Button>
+        </PopoverTrigger>
+
+        <PopoverContent
+          className="flex w-[300px] flex-col gap-2 px-2 py-4 text-text-primary"
+          align="start"
+        >
+          <Button
+            onClick={() => handleSelect(null)}
+            className="flex items-center justify-start gap-2 border-none bg-transparent text-text-primary"
+            variant={"outline"}
+          >
+            <Users className="!h-6 !w-6" />
+            {t("personalWorkspace")}
+          </Button>
+
+          <Separator className="my-2" />
+
           {!!userGroups?.length &&
             userGroups.map((g: Group) => (
-              <DropdownMenuItem
-                className="text-md whitespace-normal break-all"
+              <Button
                 key={g._id}
                 onClick={() => handleSelect(g._id)}
+                aria-label={g.name}
+                className="flex items-center justify-start gap-2 border-none bg-transparent text-text-primary"
+                variant={"outline"}
               >
-                <Avatar>
+                <Avatar className="!h-8 !w-8">
                   <AvatarImage src={g.image || "/image-placeholder.avif"} />
                 </Avatar>
                 {g.name}
-              </DropdownMenuItem>
+              </Button>
             ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
